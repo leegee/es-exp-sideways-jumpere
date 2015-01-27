14a7a303dc3ad18bb0863895619b9dd2efca8475
Lee Goddard <lee@leegoddard.net>
2015-01-27 13:01:14 +0100

    new

---
+++ htdocs/_/js/app/Grid.js
@@ -0,0 +1,44 @@
+'use strict';
+
+define(['jquery'], function (jquery) {
+
+/*  An image on a canvas.
+    The canvas is moved by CSS, the canvas is editable.
+    Faster than repainting a large iamge eery frame.
+*/
+    var Grid = function (args) {
+        console.debug('Grid.constructor enter ', arguments);
+        var self    = this;
+        this.cellSize   = args.cellSize;
+        this.width      = args.width;
+        this.height     = args.height;
+
+        this.el = jquery(
+            '<canvas id="grid"'
+            +'width="'+this.width+'" height="'+this.height+'" '
+            +'style="'
+                +'width:'+this.width+'px;height:'+this.height+'px;'
+            +'"/>'
+        );
+        jquery( document.body ).append( this.el );
+
+        this.dom = this.el.get(0);
+        this.ctx = this.dom.getContext('2d');
+
+        this.ctx.lineWidth = 1;
+        this.ctx.style = '#000000';
+        for (var y=0; y<this.height; y+=this.cellSize ){
+            this.ctx.moveTo(0,y);
+            this.ctx.lineTo(this.width,y);
+            this.ctx.stroke();
+        }
+
+       for (var x=0; x<this.width; x+=this.cellSize ){
+            this.ctx.moveTo(x,0);
+            this.ctx.lineTo(x, this.height);
+            this.ctx.stroke();
+       }
+    }
+
+    return Grid;
+});
